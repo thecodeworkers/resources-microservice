9d9d7e5b26ea89a818aa001905ab16fadb40d108
Jorge Bastidas <jorgebastidas9@gmail.com>
2021-01-20 10:32:00 -0400

    unique key

--- app/models/languages.py
+++ app/models/languages.py
@@ -1,6 +1,6 @@
 from mongoengine import Document, StringField, BooleanField, DecimalField
 
 class Languages(Document):
-    name = StringField(min_length=2,max_length=100, required=True)
-    prefix = StringField(min_length=2,max_length=5, required=True)
+    name = StringField(min_length=2,max_length=100, required=True, unique=True)
+    prefix = StringField(min_length=2,max_length=5, required=True, unique=True)
     active = BooleanField(default=True)
